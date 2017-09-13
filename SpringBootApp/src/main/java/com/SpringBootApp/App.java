eb5a4769f7b31a537b4d67935ace163795baf5f0
manjunathreddy <manjunathreddy@india.cogbooks.com>
2017-09-13 08:34:32 +0530

    SpringBoot Docker

---
+++ SpringBootApp/src/main/java/com/SpringBootApp/App.java
@@ -0,0 +1,25 @@
+package com.SpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@RestController
+@SpringBootApplication
+public class App 
+{
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
