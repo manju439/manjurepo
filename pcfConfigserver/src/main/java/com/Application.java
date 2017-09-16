3ccad7b4ba08cef141727e77fd6361941baef155
manjunathreddy <manjunathreddy@india.cogbooks.com>
2017-09-16 14:59:40 +0530

    config changes

--- pcfConfigserver/src/main/java/com/Application.java
+++ pcfConfigserver/src/main/java/com/Application.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com;
+ package cook;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
--- pcfConfigserver/src/main/java/com/Menu.java
+++ pcfConfigserver/src/main/java/com/Menu.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com;
+ package cook;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
